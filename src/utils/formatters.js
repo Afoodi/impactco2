c443e88433cb08351fd0c340241f291cd5a5d3ff
Jay DesLauriers <jay.deslauriers@gmail.com>
2022-11-29 13:55:03 +0000

    fix: use replace not replaceAll

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -28,8 +28,8 @@ export function formatNumberPrecision(value) {
 }
 export function formatName(name = '', value = 1, capital) {
   const newName = name
-    .replaceAll('[s]', value > 1 ? 's' : '')
-    .replaceAll('[x]', value > 1 ? 'x' : '')
+    .replace('[s]', value > 1 ? 's' : '')
+    .replace('[x]', value > 1 ? 'x' : '')
 
   return capital ? newName : newName.toLowerCase()
 }
